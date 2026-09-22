9c887b7da9cab17934944f8db9a1d292ffbc03f2
liviaksena-coder <liviak.sena@gmail.com>
2026-09-22 20:47:51 -0300

    Adiciona a funcionalidade de tiro à classe Jogador

--- src/jogador.py
+++ src/jogador.py
@@ -1,5 +1,7 @@
 import pyxel
 
+from src.tiro import Tiro
+
 class Jogador:
 
     def __init__(self):
@@ -8,6 +10,7 @@ class Jogador:
         self.largura = 10
         self.altura = 10
         self.velocidade = 2
+        self.tiros = []
 
     def atualizar(self):
         if pyxel.btn(pyxel.KEY_LEFT):
@@ -25,5 +28,24 @@ class Jogador:
         self.x = max(0, min(self.x, 160 - self.largura))
         self.y = max(0, min(self.y, 120 - self.altura))
 
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.atirar()
+
+        for tiro in self.tiros[:]:
+            tiro.atualizar()
+            if tiro.y < 0:
+                self.tiros.remove(tiro)
+                
+    def atirar(self):
+        x = self.x + self.largura // 2 - 1
+        y = self.y
+
+        tiro = Tiro(x, y)
+        self.tiros.append(tiro)
+
+
     def desenhar(self):
         pyxel.rect(self.x, self.y, self.largura, self.altura, 11)
+        
+        for tiro in self.tiros:
+            tiro.desenhar()
