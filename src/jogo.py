c627309e80d4d734766b8c22c9ecf02eea080290
liviaksena-coder <liviak.sena@gmail.com>
2026-09-22 21:40:40 -0300

    Corrige a atualização e desenho do inimigo no jogo

--- src/jogo.py
+++ src/jogo.py
@@ -15,10 +15,30 @@ class Jogo:
 
     def update(self):
         self.jogador.atualizar()
-        self.inimigo.atualizar()
+
+        if self.inimigo is not None:
+            self.inimigo.atualizar()
+
+            for tiro in self.jogador.tiros[:]:
+                if self.verificar_colisao(tiro, self.inimigo):
+                    self.jogador.tiros.remove(tiro)
+                    self.inimigo = None
+                    break
 
     def draw(self):
         pyxel.cls(0)
 
         self.jogador.desenhar()
-        self.inimigo.desenhar()
+
+        if self.inimigo is not None:
+            self.inimigo.desenhar()
+
+    def verificar_colisao(self, obj1, obj2):
+        if (
+            obj1.x < obj2.x + obj2.largura
+            and obj1.x + obj1.largura > obj2.x
+            and obj1.y < obj2.y + obj2.altura
+            and obj1.y + obj1.altura > obj2.y
+        ):
+            return True
+        return False
